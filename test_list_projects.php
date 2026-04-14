853387d46d75ba1ab795bb68468d292f6886df8b
Quang <quanganhbn168@gmail.com>
2026-04-14 09:51:18 +0700

    chore: remove test scripts

--- test_list_projects.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-foreach (App\Models\Project::all() as $p) {
-    echo $p->name . "\n";
-}
